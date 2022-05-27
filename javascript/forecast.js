b580aeffc30d5083e808e394963f8a0b200862eb
Mrinal Singh <mrinalsingh367@gmail.com>
2022-05-27 20:31:04 +0530

    update
    https solved

--- javascript/forecast.js
+++ javascript/forecast.js
@@ -3,7 +3,7 @@ const key = 'JTQikgBg92vTCvDUBpSTtgtXQ3MdT7XB';
 //get city information
 const getCity = async (city) => {
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
@@ -16,7 +16,7 @@ const getCity = async (city) => {
 
 const getWeather = async(id) => {
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
